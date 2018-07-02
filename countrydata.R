9429c03283210899cb9a7298cdf540f1ec8dd4f4
Tobius Saul <tobiusaolo21@gmail.com>
2018-07-01 23:18:24 -0400

    This file computes the country's overall data

---
+++ countrydata.R
@@ -0,0 +1,8 @@
+ibrary(dplyr)
+library(ggplot2)
+data<-read.csv(file.choose(),header = TRUE)
+fifa<-as.data.frame(data)
+#selecting country's data from 1993
+country_data<-fifa%>%filter(country_abrv=="GER")%>%select(rank,previous_points,rank_date)
+#graph for Germany team
+plot_bar<-ggplot(country_data,aes(x=rank_date,y=previous_points))+geom_col()+coord_flip()
