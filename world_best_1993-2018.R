b4b131b0ceb53d1847d80676fe7ef29c51986a94
EricPeter <37067613+EricPeter@users.noreply.github.com>
2018-07-11 12:50:14 +0300

    World_best_1993-2018
    Rankings for all teams from 1993 to 2018

--- world_best_1993-2018.R
+++ world_best_1993-2018.R
@@ -11,8 +11,23 @@ data<-read.csv(file.choose(),header = TRUE)
 #total perfomnce in Year 2018
 
 data<-ddply(data,.(country_full),
-                        summarize,sum=sum(previous_points))
+                        summarize,Total_Points_Earned=sum(previous_points))
 data
 #sort in descending order
-desc <- data[order(-data$sum), ]
+desc <- data[order(-data$Total_Points_Earned), ]
 desc
+#output data in table format
+library(DT)
+
+ui <- basicPage(
+  h2("The World Rankings Since 1993 to 2018 "),
+  DT::dataTableOutput("desc")
+)
+
+server <- function(input, output) {
+  output$desc = DT::renderDataTable({
+    desc
+  })
+}
+
+shinyApp(ui, server)
