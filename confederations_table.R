2434f0782d43493df822f4294b138e0d87baaceb
Tobius <tobiusaolo21@gmail.com>
2018-07-10 20:48:38 -0400

    Displays html table formatted with style and ranking by confederatioon

--- confederations_table.R
+++ confederations_table.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(ggplot2)
 library(formattable)
+library(kableExtra)
+library(knitr)
 data<-read.csv(file.choose(),header = TRUE)
 fifa<-as.data.frame(data)
 tabl_select<-fifa%>%filter(confederation=="CAF",rank_date=="6/7/2018")%>%select(rank,country_full,total_points)%>%arrange(desc(total_points))%>%slice(1:15)
@@ -10,4 +12,6 @@ choosen_data<-tabl_select[,c(1,2,3)]
 
 #change column names 
 colnames(choosen_data)<-c("RANK","COUNTRY","POINTS")
-widget_formattable<-formattable(choosen_data)
+#display the data in table format using kable
+data3<-choosen_data%>%kable()%>%kable_styling(bootstrap_options = c("striped", "hover", "condensed"))
+#widget_formattable<-formattable(choosen_data)
