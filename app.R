b583f2fd6acd821dde5a413628de8c7233331336
Tobius <tobiusaolo21@gmail.com>
2018-07-25 02:49:40 -0400

    final project app

--- app.R
+++ app.R
@@ -6,25 +6,34 @@ library(png)
 library(dplyr)
 library(formattable)
 library(markdown)
+library(stringr)
 library(lubridate)
 library(plyr)
+library(rsconnect)
 library(tidyr)
 
+
 #get file
-data<-read.csv(file.choose(),header = TRUE)
+#data<-read.csv(file.choose(),header = TRUE)
+
+#data<-read.csv(file="data/fifa_ranking.csv")
 fifa<-as.data.frame(data)
+data4<-data%>%
+  select(rank,country_full,rank_change,previous_points,rank_date)
+subset <- data4%>%select(rank_date)
+subset_date <- format(as.Date(subset$rank_date, format="%m/%d/%Y"),"%Y")
+subset_data <-cbind(data4,subset_date)
 
 ui <- dashboardPage(
+  ###applying the styles to shiny
+  
+  ####end of style
 
   dashboardHeader(title = "FiFA World Ranking System"),
   dashboardSidebar(
     
     #the logo for the site
     imageOutput("image1",height = 30),
-    
-    
-   
-    
     tags$hr(),
     
     #the sidebar menu
@@ -38,7 +47,7 @@ ui <- dashboardPage(
              menuSubItem("OceanianTable",tabName = "oceantable"),
              menuSubItem("Asia Table",tabName = "asiatable")),
     
-    menuItem( tags$h4("Conferderation"),
+    menuItem( tags$h4(icon("globe"),"Conferderation"),
     
       menuSubItem("UEFA",tabName = "UEFA"),
       menuSubItem("CAF", tabName = "CAF"),
@@ -47,11 +56,14 @@ ui <- dashboardPage(
       menuSubItem("OFC", tabName = "OFC"),
       menuSubItem("CONCACAF",tabName = "CONCACAF")
     ),
-    menuItem("Summaries",tabName = "summary"),
-    menuItem("table",tabName = "table"),
-    menuItem("Graphs",tabName = "graph"),
-  
-    menuItem("About",tabName="about_tab",icon=icon("info"))
+    menuItem("Summaries",tabName = "summary",icon=icon("list-alt")),
+    menuItem("Tables",tabName = "table",icon=icon("table")),
+    menuItem("Graphs",tabName = "graph",icon=icon("chart-area")),
+  menuItem("Leaders",tabName="table2",icon=icon("chess-knight")),
+    menuItem("About",tabName="about_tab",icon=icon("info")),
+  fileInput('datafile','Choose file',
+            accept = c('text/csv','text/comma-separated-values,text/plain')
+            )
   )
   ),
   #the skin color of the dashboard
@@ -99,45 +111,31 @@ ui <- dashboardPage(
               actionButton("value5","PLOT"),
               plotOutput("concacafbest")),
       tabItem(tabName = "table",
-              
+                   
                   #select input to display confederation, country data 
                   selectInput("Data","Choose Data:",
-                              choices = c("World Best","UEFA","CAF","OFC","AFC","CONMEBOL","CONCACAF","Country Data")),
-                  conditionalPanel(condition = "input.Data == 'World Best'",
-                                   column(6,textInput("date","insert date","6/7/2018")),
+                              choices = c("World Best","UEFA","CAF","OFC","AFC","CONMEBOL","CONCACAF")),
+                  conditionalPanel(condition = "input.Data == 'World Best'"),
+                  
+                  conditionalPanel(condition = "input.Data == 'UEFA'"),
+                                
+                  conditionalPanel(condition = "input.Data == 'CAF'"),
                                    
-                                   column(6,textInput("range","insert range",50))),
                   
-                  conditionalPanel(condition = "input.Data == 'UEFA'",
-                                   column(6,textInput("date","insert date","6/7/2018")),
-                                   column(6,textInput("range","insert range",10))),
+                  conditionalPanel(condition = "input.Data == 'OFC'"),
+                                 
                   
-                  conditionalPanel(condition = "input.Data == 'CAF'",
-                                   column(6,textInput("date","insert date","6/7/2018")),
-                                   column(6,textInput("range","insert range",10))),
+                  conditionalPanel(condition = "input.Data == 'CONMEBOL'"),
+                                  
                   
-                  conditionalPanel(condition = "input.Data == 'OFC'",
-                                   column(6,textInput("date","insert date","6/7/2018")),
-                                   column(6,textInput("range","insert range",10))),
+                  conditionalPanel(condition = "input.Data == 'CONCACAF'"),
+                                 
                   
-                  conditionalPanel(condition = "input.Data == 'CONMEBOL'",
-                                   column(6,textInput("date","insert date","6/7/2018")),
-                                   column(6,textInput("range","insert range",10))),
-                  
-                  conditionalPanel(condition = "input.Data == 'CONCACAF'",
-                                   column(6,textInput("date","insert date","6/7/2018")),
-                                   column(6,textInput("range","insert range",10))),
-                  
-                  conditionalPanel(condition = "input.Data == 'AFC'",
-                                   column(6,textInput("date","insert date","6/7/2018")),
-                                   column(6,textInput("range","insert range",10))),
-                  
-                  conditionalPanel(condition = "input.Data == 'Country Data'",
-                                   column(6,textInput("country","Choose Country","GER")),
-                                   column(6,textInput("yr","Choose Year","2018"))),
+                  conditionalPanel(condition = "input.Data == 'AFC'"),
+              DT::dataTableOutput("plotTable")
                   
                   
-                  tableOutput("plotTable")
+                  #tableOutput("plotTable")
                   
               
               ),
@@ -148,12 +146,15 @@ ui <- dashboardPage(
               plotOutput("top3"),
               plotOutput("top32")
               ),
+      tabItem(tabName = "table2", uiOutput("table_html")),
       tabItem(tabName = "search",
               verbatimTextOutput("search")
       ),
       tabItem(tabName = "graph",
               selectInput("country", label = strong("Trend index"),
                           choices = unique(fifa$country_full),selected = "Brazil"),
+              selectInput('date', label = strong("Select Date"),
+                          choices = unique(subset_data$subset_date),selected = "1999"),
               textOutput("Department"),
               plotOutput("country")
       ),
@@ -175,6 +176,14 @@ ui <- dashboardPage(
   
 
 server <- function(input,output){
+  #readind file from the pc 
+  filedata<-reactive({
+    infile<-input$datafile
+    if(is.null(infile)){
+      return(NULL)
+    }
+    data<-read.csv(infile$datapath)
+  })
   #rendering formatable tables ##############################################
     output$mytable=renderFormattable({
       world_data1<-fifa%>%filter(rank<=50,rank_date=="6/7/2018")%>%select(rank,country_full,confederation,previous_points,rank_change,cur_year_avg,last_year_avg,two_year_ago_avg,three_year_ago_avg,total_points)%>%arrange(desc(total_points))%>%slice(1:216)
@@ -244,17 +253,29 @@ server <- function(input,output){
     renderText(input$country)
   )
   }
+    output$date <- renderText({
+      paste("input$date is", as.character(input$date))
+    })
   output$country<-renderPlot({
     text_data<-input$country
-    dta<-data%>%filter(country_full==text_data)%>%
-      select(rank,country_full,rank_change,previous_points)%>%
+    datex <-input$date
+    
+    dtax<-subset_data%>%filter(country_full==text_data)%>%
+      select(rank,country_full,rank_change,previous_points,rank_date,subset_date)%>%
       arrange(desc(previous_points))
     
-    ggplot(data=dta, aes_string(x=dta$previous_points,y=dta$rank_change))  +
-      stat_summary( geom = "bar",colour="#56B4E9",fill="#56B4E9") +
+    dta <- dtax %>% filter(subset_date ==datex)%>%
+      select(rank,country_full,rank_change,previous_points,rank_date,subset_date)
+    
+    
+    ggplot(data=dta, aes_string(y=dta$previous_points,x=dta$rank_date,fill=dta$rank_date))  +
+      stat_summary( geom = "bar") +
       geom_bar(stat="identity") +
-      labs(title=input$country, y ="Rank_change",x="Total Points Earned") +
-      theme_classic() +
+      labs(title=input$country, y ="Total Points Earned",x="Year") +
+      scale_fill_hue(name="Supplement type", # Legend label, use darker colors
+                     breaks=c("OJ", "VC"),
+                     labels=c("Orange juice", "Ascorbic acid")) +
+      theme_minimal() +
       theme(plot.title = element_text(hjust = 0.5))
   })
  
@@ -277,36 +298,50 @@ server <- function(input,output){
   
   
   #display table of world best
-  output$plotTable <- renderTable({
+  output$plotTable =DT::renderDataTable({
     choosen_data <- input$Data
     Date_input <-  input$date
     Range <-  input$range
     yearly_data<-input$yr
     country_spec<-input$country
     if(choosen_data == "World Best"){
-    world_data<-fifa%>%filter(rank<=Range,rank_date==Date_input)%>%select(rank,country_full,total_points)%>%arrange(desc(total_points))%>%slice(1:Range)
-    best_data<-world_data[,c(1,2,3)]
-    colnames(best_data)<-c("RANK","COUNTRY","POINTS")
-    widget_formattable<-formattable(best_data)
+    world_data<-fifa%>%select(rank,country_full,total_points)%>%arrange(desc(total_points))
+     ### slice(1:Range)
+    agg2<-split(world_data,world_data$country_full)
+    trial1 <-do.call(rbind,lapply(agg2, function(chunk) chunk[which.max(chunk$total_points),]))
+    world_data<-ddply(trial1,
+                       .(rank),function(x)x[x$total_points==max(x$total_points),]
+    )
+    #best_data<-world_data[,c(1,2,3)]
+    #colnames(best_data)<-c("RANK","COUNTRY","POINTS")
+    #widget_formattable<-fobest_data)
+    datatable(world_data)
    
     
     }else if(choosen_data%in%c("UEFA","CAF","CONCACAF","OFC","AFC","CONMEBOL")){#choosen_data == "UEFA"){
-      tabl_select<-fifa%>%filter(confederation==choosen_data,rank_date==Date_input)%>%select(rank,country_full,total_points)%>%arrange(desc(total_points))%>%slice(1:Range)
+      tabl_select1<-fifa%>%filter(confederation==choosen_data)%>%select(rank,country_full,total_points)%>%arrange(desc(total_points))
+      agg<-split(tabl_select1,tabl_select1$country_full)
+      trial <-do.call(rbind,lapply(agg, function(chunk) chunk[which.max(chunk$total_points),]))
+      tabl_select<-ddply(trial,
+                         .(rank),function(x)x[x$total_points==max(x$total_points),]
+                         )
+      #%>%slice(1:Range)
       #selecting the columns to display
-      cho_data<-tabl_select[,c(1,2,3)]
+     # cho_data<-tabl_select[,c(1,2,3)]
       #The display the table
-      
+      datatable(tabl_select)
       #change column names 
-      colnames(cho_data)<-c("WORLD-RANK","COUNTRY","POINTS")
-      widget_formattable<-formattable(cho_data)
+      #colnames(cho_data)<-c("WORLD-RANK","COUNTRY","POINTS")
+      #widget_formattable<-formattable(cho_data)
     }else if(choosen_data=="Country Data"){
       country_data<-fifa%>%filter(filtered_date<-substring(rank_date,6)==yearly_data,country_abrv==country_spec)%>%select(rank,total_points,rank_change)
-      cont_data<-country_data[,c(1,2,3)]
-      #The display the table
+      #cont_data<-country_data[,c(1,2,3)]
       
+      #The display the table
+      datatable(country_data)
       #change column names 
-      colnames(cont_data)<-c("RANK","POINTS","CHANGE")
-      widget_formattable<-formattable(cont_data)
+      #colnames(cont_data)<-c("RANK","POINTS","CHANGE")
+      #widget_formattable<-formattable(cont_data)
     }
 
   },striped = T,width = "600",align ="c")
@@ -382,6 +417,10 @@ server <- function(input,output){
   output$abouts<-reactiveUI(function(){
     file_to_show='aboutus.Rhtml'
     HTML(readLines(file_to_show))
+  })
+  output$table_html<-reactiveUI(function(){
+    file_to_show2="leaders.Rhtml"
+    HTML(readLines(file_to_show2))
   })
   #choice made of the select option
  # output$result <- renderText(paste(input$conferderation))
--- conferation_best.R
+++
@@ -1,9 +0,0 @@
-library(dplyr)
-library(ggplot2)
-data<-read.csv(file.choose(),header = TRUE)
-fifa<-as.data.frame(data)
-#condeferations best
-africa_best<-fifa%>%filter(confederation=="UEFA",rank_date=="6/7/2018")%>% select(rank,country_full,total_points)%>%slice(1:10)
-#plot the bar graph for africas best 
-data_plot<-ggplot(africa_best,aes(x=country_full,y=total_points))+geom_col()+coord_flip()
-data_plot
